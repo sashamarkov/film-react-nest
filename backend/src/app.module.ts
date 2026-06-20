111dffe2200f3ab24da5d61d2b20203a295a4490
Sasha Markov <sasha.markov.mail@gmail.com>
2026-06-20 12:02:33 +0300

    fix: удалил полифил для crypto

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,7 +4,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { APP_FILTER } from '@nestjs/core';
 import * as path from 'node:path';
-import * as crypto from 'node:crypto';
 import { FilmsModule } from './films/films.module';
 import { OrderModule } from './order/order.module';
 import { HttpExceptionFilter } from './common/http-exception.filter';
@@ -12,10 +11,6 @@ import { validationSchema } from './config/validation.schema';
 import { Film } from './entities/film.entity';
 import { Schedule } from './entities/schedule.entity';
 import { RepositoryModule } from './repository/repository.module';
-
-if (typeof globalThis.crypto === 'undefined') {
-  (globalThis as any).crypto = crypto;
-}
 
 @Module({
   imports: [
